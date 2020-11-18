201d6cfb5101ab3ef6f18768c73c8cb38d7ca340
mawir157 <jamesmthompson2@gmail.com>
2020-11-18 17:40:43 +0000

    Improve test shell script

--- full.sh
+++ full.sh
@@ -1,12 +1,21 @@
 make -B
-rm cipher.txt
-./mceliese -g 10 30
+rm -f cipher.txt
+rm -f decoded.txt
+./mceliese -g 8 56
 ./mceliese -e test.txt public.mce >> cipher.txt
+./mceliese -d cipher.txt private.mce >> decoded.txt
 
-echo "Plaintext"
-cat test.txt
-
-echo
-echo "Decoded text"
-./mceliese -d cipher.txt private.mce
-
+diff test.txt decoded.txt
+DIFF=$(diff test.txt decoded.txt) 
+if [ "$DIFF" != "" ] 
+then
+  echo "There was an error:"
+  echo "Plaintext"
+	cat test.txt
+	echo
+  echo "Decoded text"
+	cat decoded.txt
+  echo $DIFF
+else
+	echo "Completed encryption/decrptyion circuit with no errors. :)"
+fi
