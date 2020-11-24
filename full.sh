4c7047434d5d3c345c53b4659da2ed78142b0d22
mawir157 <jamesmthompson2@gmail.com>
2020-11-24 17:43:36 +0000

    Improve the shell testing script

--- full.sh
+++ full.sh
@@ -1,8 +1,23 @@
+if [ $# -gt 0 ]
+then
+	dim=$1
+else
+	dim=8
+fi
+
+if [ $# -gt 1 ]
+then
+	extra_bits=$2
+else
+	extra_bits=48
+fi
+
 make -B
 echo "Compilation    OK!"
 rm -f cipher.txt
 rm -f decoded.txt
-./mceliese -g 8 48
+
+./mceliese -g $dim $extra_bits
 echo "Key generation OK!"
 ./mceliese -e test.txt public.mce >> cipher.txt
 echo "Encryption     OK!"
